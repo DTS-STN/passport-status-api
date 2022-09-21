c7e42236d4c2cdf6d0b00d46bf22b32d7e948c99
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2022-09-21 12:33:55 -0300

    Use ResourceNotFoundException generic class

---
+++ src/main/java/ca/gov/dtsstn/passport/api/web/exception/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package ca.gov.dtsstn.passport.api.web.exception;
+
+import org.springframework.core.NestedRuntimeException;
+
+/**
+ * @author Sébastien Comeau (sebastien.comeau@hrsdc-rhdcc.gc.ca)
+ */
+public class ResourceNotFoundException extends NestedRuntimeException {
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+
+	public ResourceNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
