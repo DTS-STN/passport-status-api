60ba6a6a15da0dc03a190b3244cfb63757de803b
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-09-22 14:44:56 -0230

    Start embedded mongo if no connection string provided

--- src/main/java/ca/gov/dtsstn/passport/api/config/DataSourceConfig.java
+++ src/main/java/ca/gov/dtsstn/passport/api/config/DataSourceConfig.java
@@ -6,8 +6,14 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
@@ -46,4 +52,17 @@ public class DataSourceConfig {
 		log.info("Creating 'mongoCustomConversions' bean");
 		return new MongoCustomConversions(List.of(dateToLocalDateConverter, localDateToDateConverter));
 	}
+
+	@Configuration
+	@Profile("!test")
+	@ConditionalOnProperty("spring.data.mongodb.uri")
+	@EnableAutoConfiguration(exclude = { EmbeddedMongoAutoConfiguration.class })
+	class EmbeddedMongoConfiguration {
+
+		@Bean ApplicationListener<ContextRefreshedEvent> embeddedMongoConfigurationNotifier() {
+			return args -> log.info("Property spring.data.mongodb.uri has a value; disabling embedded Mongo...");
+		}
+
+	}
+
 }
