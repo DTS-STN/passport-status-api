183ad39b525853844ea62ec3cfc20af7043d5779
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-09-24 12:07:26 -0230

    Add AbstractModelAssembler

--- src/main/java/ca/gov/dtsstn/passport/api/web/assembler/PassportStatusModelAssembler.java
+++ src/main/java/ca/gov/dtsstn/passport/api/web/assembler/PassportStatusModelAssembler.java
@@ -3,11 +3,8 @@ package ca.gov.dtsstn.passport.api.web.assembler;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
-import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
@@ -22,18 +19,14 @@ import ca.gov.dtsstn.passport.api.web.model.PassportStatusModel;
  * @author Greg Baker (gregory.j.baker@hrsdc-rhdcc.gc.ca)
  */
 @Component
-public class PassportStatusModelAssembler extends RepresentationModelAssemblerSupport<PassportStatus, PassportStatusModel> {
-
-	private final PagedResourcesAssembler<PassportStatus> pagedResourcesAssembler;
+public class PassportStatusModelAssembler extends AbstractModelAssembler<PassportStatus, PassportStatusModel> {
 
 	private final PassportStatusModelMapper passportStatusModelMapper;
 
 	public PassportStatusModelAssembler(PagedResourcesAssembler<PassportStatus> pagedResourcesAssembler, PassportStatusModelMapper passportStatusModelMapper) {
-		super(PassportStatusController.class, PassportStatusModel.class);
+		super(PassportStatusController.class, PassportStatusModel.class, pagedResourcesAssembler);
 
-		Assert.notNull(pagedResourcesAssembler, "pagedResourcesAssembler is required; it must not be null");
 		Assert.notNull(passportStatusModelMapper, "passportStatusModelMapper is required; it must not be null");
-		this.pagedResourcesAssembler = pagedResourcesAssembler;
 		this.passportStatusModelMapper = passportStatusModelMapper;
 	}
 
@@ -54,18 +47,6 @@ public class PassportStatusModelAssembler extends RepresentationModelAssemblerSu
 		final var searchLink = linkTo(searchMethod).slash(searchQueryTemplate).withRel("search").expand(passportStatus.getDateOfBirth(), passportStatus.getFileNumber(), passportStatus.getFirstName(), passportStatus.getLastName());
 
 		return createModelWithId(passportStatus.getId(), passportStatus).add(searchLink);
-	}
-
-	@SuppressWarnings({ "unchecked" })
-	public PagedModel<PassportStatusModel> toEmptyPagedModel(Page<PassportStatus> passportStatuses) {
-		return (PagedModel<PassportStatusModel>) pagedResourcesAssembler.toEmptyModel(passportStatuses, PassportStatusModel.class);
-	}
-
-	public PagedModel<PassportStatusModel> toPagedModel(Page<PassportStatus> passportStatuses) {
-		Assert.notNull(passportStatuses, "passportStatuses is required; it must not be null");
-		return passportStatuses.isEmpty()
-			? toEmptyPagedModel(passportStatuses)
-			: pagedResourcesAssembler.toModel(passportStatuses, this);
 	}
 
 }
