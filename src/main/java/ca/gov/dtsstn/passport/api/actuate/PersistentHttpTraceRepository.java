5486950b79581bd6553734272191340abe656b1b
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-09-24 12:59:13 -0230

    Clean up HttpTraceRepository stuff

--- src/main/java/ca/gov/dtsstn/passport/api/actuate/PersistentHttpTraceRepository.java
+++ src/main/java/ca/gov/dtsstn/passport/api/actuate/PersistentHttpTraceRepository.java
@@ -1,17 +1,16 @@
 package ca.gov.dtsstn.passport.api.actuate;
 
-import java.util.List;
-
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.boot.actuate.trace.http.HttpTrace;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
-import ca.gov.dtsstn.passport.api.actuate.mapper.HttpTraceMapper;
 import ca.gov.dtsstn.passport.api.data.HttpTraceRepository;
+import ca.gov.dtsstn.passport.api.data.document.HttpTraceDocument;
 
 /**
  * A persistent implementation of {@link org.springframework.boot.actuate.trace.http.HttpTraceRepository}.
@@ -20,33 +19,53 @@ import ca.gov.dtsstn.passport.api.data.HttpTraceRepository;
  */
 @Repository
 @ConfigurationProperties("application.http-trace-repository")
-public class PersistentHttpTraceRepository implements org.springframework.boot.actuate.trace.http.HttpTraceRepository {
+public class PersistentHttpTraceRepository extends InMemoryHttpTraceRepository {
 
 	private final HttpTraceRepository httpTraceRepository;
 
 	private final HttpTraceMapper httpTraceMapper;
 
-	private int pageSize = 100;
-
 	public PersistentHttpTraceRepository(HttpTraceMapper httpTraceMapper, HttpTraceRepository httpTraceRepository) {
+		Assert.notNull(httpTraceMapper, "httpTraceMapper is required; it must not be null");
+		Assert.notNull(httpTraceRepository, "httpTraceRepository is required; it must not be null");
 		this.httpTraceMapper = httpTraceMapper;
 		this.httpTraceRepository = httpTraceRepository;
 	}
 
 	@Override
-	public List<HttpTrace> findAll() {
-		final var pageRequest = PageRequest.of(0, pageSize, Sort.by(Direction.DESC,"timestamp"));
-		return httpTraceRepository.findAll(pageRequest).map(httpTraceMapper::fromDocument).getContent();
-	}
-
-	@Override
+	@SuppressWarnings({ "java:S4449" })
 	public void add(HttpTrace httpTrace) {
+		super.add(httpTrace);
 		httpTraceRepository.save(httpTraceMapper.toDocument(httpTrace));
 	}
 
-	public void setPageSize(int pageSize) {
-		Assert.isTrue(pageSize >= 0, "application.http-trace-repository.page-size must be greater than or equal to zero");
-		this.pageSize = pageSize;
+	@Override
+	public void setCapacity(int capacity) {
+		Assert.isTrue(capacity >= 0, "application.http-trace-repository.capacity must be greater than or equal to zero");
+		super.setCapacity(capacity);
+	}
+
+	@Mapper
+	interface HttpTraceMapper {
+
+		@Nullable
+		@Mapping(target = "id", ignore = true)
+		@Mapping(target = "principalName", source = "principal.name")
+		@Mapping(target = "sessionId", source = "session.id")
+		@Mapping(target = "requestMethod", source = "request.method")
+		@Mapping(target = "requestUri", source = "request.uri")
+		@Mapping(target = "requestHeaders", source = "request.headers")
+		@Mapping(target = "requestRemoteAddress", source = "request.remoteAddress")
+		@Mapping(target = "putRequestHeaders", ignore = true)
+		@Mapping(target = "putAllRequestHeaders", ignore = true)
+		@Mapping(target = "responseStatus", source = "response.status")
+		@Mapping(target = "responseHeaders", source = "response.headers")
+		@Mapping(target = "timeTakenMillis", source = "timeTaken")
+		// for some reason, MapStruct thinks these are target fields 🤷
+		@Mapping(target = "putResponseHeaders", ignore = true)
+		@Mapping(target = "putAllResponseHeaders", ignore = true)
+		HttpTraceDocument toDocument(@Nullable HttpTrace httpTrace);
+
 	}
 
 }
