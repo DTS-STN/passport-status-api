eddbec1749e4ad078b3733868d8d7f6679337f99
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-09-24 19:03:15 -0230

    Add PersistentHttpTraceRepositoryTests

---
+++ src/test/java/ca/gov/dtsstn/passport/api/actuate/PersistentHttpTraceRepositoryTests.java
@@ -0,0 +1,55 @@
+package ca.gov.dtsstn.passport.api.actuate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+import java.net.URI;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.actuate.trace.http.HttpTrace;
+import org.springframework.boot.actuate.trace.http.HttpTrace.Principal;
+import org.springframework.boot.actuate.trace.http.HttpTrace.Request;
+import org.springframework.boot.actuate.trace.http.HttpTrace.Response;
+import org.springframework.boot.actuate.trace.http.HttpTrace.Session;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import ca.gov.dtsstn.passport.api.actuate.PersistentHttpTraceRepository.HttpTraceMapper;
+import ca.gov.dtsstn.passport.api.data.HttpTraceRepository;
+
+/**
+ * @author Greg Baker (gregory.j.baker@hrsdc-rhdcc.gc.ca)
+ */
+@ExtendWith({ MockitoExtension.class })
+class PersistentHttpTraceRepositoryTests {
+
+	PersistentHttpTraceRepository persistentHttpTraceRepository;
+
+	@Mock HttpTraceRepository httpTraceRepository;
+
+	@BeforeEach void beforeEach() {
+		this.persistentHttpTraceRepository = new PersistentHttpTraceRepository(Mappers.getMapper(HttpTraceMapper.class), httpTraceRepository);
+	}
+
+	@Test void testAdd() {
+		final var request = new Request("GET", URI.create("https://example.com/"), new HttpHeaders(), "127.0.0.1");
+		final var response = new Response(HttpStatus.OK.value(), new HttpHeaders());
+		final var timestamp = LocalDate.of(2000, 01, 01).atStartOfDay(ZoneOffset.UTC).toInstant();
+		final var principal = new Principal("user");
+		final var session = new Session("00000000-0000-0000-0000-000000000000");
+		final var timeTaken = Duration.ofMinutes(1).toMillis();
+
+		persistentHttpTraceRepository.add(new HttpTrace(request, response, timestamp, principal, session, timeTaken));
+
+		verify(httpTraceRepository).save(any());
+	}
+
+}
