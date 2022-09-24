8e24c9041961350d89a0ba2d6666464f371fbefa
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-09-24 12:21:47 -0230

    Add swagger doc to PassportStatusModel

--- src/main/java/ca/gov/dtsstn/passport/api/web/model/PassportStatusModel.java
+++ src/main/java/ca/gov/dtsstn/passport/api/web/model/PassportStatusModel.java
@@ -1,11 +1,15 @@
 package ca.gov.dtsstn.passport.api.web.model;
 
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDate;
+
 import org.immutables.value.Value.Immutable;
 import org.immutables.value.Value.Style;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
+import org.springframework.lang.Nullable;
 
-import ca.gov.dtsstn.passport.api.service.domain.PassportStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
@@ -18,4 +22,57 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @SuppressWarnings({ "immutables", "serial" })
 @Style(passAnnotations = { Relation.class })
 @Relation(collectionRelation = "passportStatuses", itemRelation = "passportStatus")
-public abstract class PassportStatusModel extends RepresentationModel<PassportStatusModel> implements PassportStatus {}
+public abstract class PassportStatusModel extends RepresentationModel<PassportStatusModel> implements Serializable {
+
+	public enum Status {
+
+		// TODO :: GjB :: these are obviously made up
+		ACCEPTED, PROCESSING, REJECTED, COMPLETED;
+
+	}
+
+	@Nullable
+	@Schema(description = "The internal database ID of the passport status.", example = "476743eac593f65fd6964a15")
+	public abstract String getId();
+
+	@Nullable
+	@Schema(description = "The creator of the resource.", example = "Passport Status API")
+	public abstract String getCreatedBy();
+
+	@Nullable
+	@Schema(description = "The creation timestamp of the resource in ISO-8601 format.", example = "2000-01-01T00:00:00.000Z")
+	public abstract Instant getCreatedDate();
+
+	@Nullable
+	@Schema(description = "The last modifior of the resource.", example = "Passport Status API")
+	public abstract String getLastModifiedBy();
+
+	@Nullable
+	@Schema(description = "The last modification timestamp of the resource in ISO-8601 format.", example = "2000-01-01T00:00:00.000Z")
+	public abstract Instant getLastModifiedDate();
+
+	@Nullable
+	@Schema(description = "The current version of the resource. Used to enforce opportunistic locking during updates.", example = "0")
+	public abstract Long getVersion();
+
+	@Nullable
+	@Schema(description = "The electronic service request file number.", example = "ABCD1234")
+	public abstract String getFileNumber();
+
+	@Nullable
+	@Schema(description = "The first name of the passport applicant.", example = "John")
+	public abstract String getFirstName();
+
+	@Nullable
+	@Schema(description = "The last name of the passport applicant.", example = "Doe")
+	public abstract String getLastName();
+
+	@Nullable
+	@Schema(description = "The date of birth of the passport applicant in ISO-8601 format.", example = "2000-01-01")
+	public abstract LocalDate getDateOfBirth();
+
+	@Nullable
+	@Schema(description = "The status of the passport application.")
+	public abstract Status getStatus();
+
+}
