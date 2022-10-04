09316164f8354b418b2dcfd26c60bc9a40c593ae
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-10-04 20:25:17 -0230

    Fix error controller ordering

--- src/main/java/ca/gov/dtsstn/passport/api/web/AuthenticationErrorController.java
+++ src/main/java/ca/gov/dtsstn/passport/api/web/AuthenticationErrorController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -40,6 +42,7 @@ import ca.gov.dtsstn.passport.api.web.model.error.ImmutableAuthenticationErrorMo
  * @author Greg Baker (gregory.j.baker@hrsdc-rhdcc.gc.ca)
  */
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE) // this should fire before ApiErrorController
 public class AuthenticationErrorController implements AccessDeniedHandler, AuthenticationEntryPoint {
 
 	private static final Logger log = LoggerFactory.getLogger(AuthenticationErrorController.class);
