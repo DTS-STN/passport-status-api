42372f4111f8c1c3e3dc4ad562aaccb7c432209f
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-10-16 18:21:27 -0230

    Tweak Spring Security

--- src/main/java/ca/gov/dtsstn/passport/api/security/AuthoritiesConstants.java
+++ src/main/java/ca/gov/dtsstn/passport/api/security/AuthoritiesConstants.java
@@ -5,12 +5,18 @@ package ca.gov.dtsstn.passport.api.security;
  *
  * @author Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
  */
-public abstract class AuthoritiesConstants { // NOSONAR
+public final class AuthoritiesConstants {
 
 	public static final String APPLICATION_MANAGE = "Application.Manage";
 
 	public static final String PASSPORTSTATUS_READ = "PassportStatus.Read";
 
+	public static final String PASSPORTSTATUS_READ_ALL = "PassportStatus.Read.All";
+
 	public static final String PASSPORTSTATUS_WRITE = "PassportStatus.Write";
+
+	public static final String PASSPORTSTATUS_WRITE_ALL = "PassportStatus.Write.All";
+
+	private AuthoritiesConstants() {}
 
 }
