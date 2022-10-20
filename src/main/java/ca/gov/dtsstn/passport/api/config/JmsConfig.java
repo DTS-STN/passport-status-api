66fa22574c96ded50552f4770590894bce6f2fa8
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2022-10-20 13:16:37 -0300

    Use spring managed  ObjectMapper

--- src/main/java/ca/gov/dtsstn/passport/api/config/JmsConfig.java
+++ src/main/java/ca/gov/dtsstn/passport/api/config/JmsConfig.java
@@ -2,6 +2,7 @@ package ca.gov.dtsstn.passport.api.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -9,23 +10,20 @@ import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class JmsConfig {
 
 	private static final Logger log = LoggerFactory.getLogger(JmsConfig.class);
 
+	@Autowired ObjectMapper objectMapper;
+
 	@Bean MessageConverter jacksonJmsMessageConverter() {
 		log.info("Creating 'jacksonJmsMessageConverter' bean");
-
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.registerModule(new JavaTimeModule());
-
-		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		final var converter = new MappingJackson2MessageConverter();
 		converter.setTargetType(MessageType.TEXT);
 		converter.setTypeIdPropertyName("_type");
-		converter.setObjectMapper(mapper);
+		converter.setObjectMapper(objectMapper);
 		return converter;
 	}
 
