fd6a27624605eb4c51b6fad70e009f01eb33711b
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2022-10-20 13:24:21 -0300

    Use a managed ObjectMapper instead of spring

--- src/main/java/ca/gov/dtsstn/passport/api/config/JmsConfig.java
+++ src/main/java/ca/gov/dtsstn/passport/api/config/JmsConfig.java
@@ -2,7 +2,6 @@ package ca.gov.dtsstn.passport.api.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -16,10 +15,12 @@ public class JmsConfig {
 
 	private static final Logger log = LoggerFactory.getLogger(JmsConfig.class);
 
-	@Autowired ObjectMapper objectMapper;
-
 	@Bean MessageConverter jacksonJmsMessageConverter() {
 		log.info("Creating 'jacksonJmsMessageConverter' bean");
+
+		final var objectMapper = new ObjectMapper();
+		objectMapper.findAndRegisterModules();
+
 		final var converter = new MappingJackson2MessageConverter();
 		converter.setTargetType(MessageType.TEXT);
 		converter.setTypeIdPropertyName("_type");
