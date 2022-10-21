ae22c541fd91843966bf7950e515f60353598205
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2022-10-21 10:55:04 -0230

    Add NIEM model mapper and unit test (#31)

---
+++ src/main/java/ca/gov/dtsstn/passport/api/web/model/CreateCertificateApplicationRequestModelMapper.java
@@ -0,0 +1,128 @@
+package ca.gov.dtsstn.passport.api.web.model;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+
+import ca.gov.dtsstn.passport.api.service.domain.PassportStatus;
+
+/**
+ * @author Greg Baker (gregory.j.baker@hrsdc-rhdcc.gc.ca)
+ */
+@Mapper
+public interface CreateCertificateApplicationRequestModelMapper {
+
+	public static final String APPLICATION_REGISTER_SID = "Application Register SID";
+
+	public static final String FILE_NUMBER = "File Number";
+
+	// TODO :: GjB :: remove this once actual code → status mappings are known
+	final Map<String, PassportStatus.Status> statusMap = Map.of(
+		"1", PassportStatus.Status.APPROVED,
+		"2", PassportStatus.Status.IN_EXAMINATION,
+		"3", PassportStatus.Status.REJECTED
+	);
+
+	@Nullable
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "createdBy", ignore = true)
+	@Mapping(target = "createdDate", ignore = true)
+	@Mapping(target = "lastModifiedBy", ignore = true)
+	@Mapping(target = "lastModifiedDate", ignore = true)
+	@Mapping(target = "applicationRegisterSid", source = "certificateApplication.certificateApplicationIdentifications", qualifiedByName = { "findApplicationRegisterSid" })
+	@Mapping(target = "dateOfBirth", source = "certificateApplication.certificateApplicationApplicant.birthDate.date")
+	@Mapping(target = "email", source = "certificateApplication.certificateApplicationApplicant.personContactInformation.contactEmailId")
+	@Mapping(target = "fileNumber", source = "certificateApplication.certificateApplicationIdentifications", qualifiedByName = { "findFileNumber" })
+	@Mapping(target = "firstName", source = "certificateApplication.certificateApplicationApplicant.personName.personGivenNames", qualifiedByName = { "getFirstElement" })
+	@Mapping(target = "lastName", source = "certificateApplication.certificateApplicationApplicant.personName.personSurname")
+	@Mapping(target = "status", source = "certificateApplication.certificateApplicationStatus", qualifiedByName = { "toStatus" })
+	PassportStatus toDomain(@Nullable CreateCertificateApplicationRequestModel createCertificateApplicationRequest);
+
+	/**
+	 * Map a {@link CertificateApplicationApplicantModel} to a {@link PassportStatus.Status}. Returns {@code null} if
+	 * {@code certificateApplicationStatusModel} is null or the status cannot be found.
+	 */
+	@Nullable
+	@Named("toStatus")
+	default PassportStatus.Status toStatus(@Nullable CertificateApplicationStatusModel certificateApplicationStatus) {
+		return Optional.ofNullable(certificateApplicationStatus)
+			.map(CertificateApplicationStatusModel::getStatusCode)
+			.map(statusMap::get)
+			.orElse(null);
+	}
+
+	/**
+	 * Finds the application registration SID element within {@code certificateApplicationIdentifications}. Returns null
+	 * if not found or {@code certificateApplicationIdentifications} is null.
+	 */
+	@Nullable
+	@Named("findApplicationRegisterSid")
+	default String findApplicationRegisterSid(@Nullable Iterable<CertificateApplicationIdentificationModel> certificateApplicationIdentifications) {
+		return findCertificateApplicationIdentification(certificateApplicationIdentifications, APPLICATION_REGISTER_SID);
+	}
+
+	/**
+	 * Finds the {@code CertificateApplicationIdentificationModel} that has a matching {@code identificationCategoryText}
+	 * value. Returns {@code null} if {@code certificateApplicationIdentifications} is null.
+	 */
+	@Nullable
+	default String findCertificateApplicationIdentification(@Nullable Iterable<CertificateApplicationIdentificationModel> certificateApplicationIdentifications, String identificationCategoryText) {
+		Assert.hasText(identificationCategoryText, "identificationCategoryText is required; it must not be null or blank");
+		return stream(certificateApplicationIdentifications)
+			.filter(hasIdentificationCategoryText(identificationCategoryText))
+			.map(CertificateApplicationIdentificationModel::getIdentificationId)
+			.findFirst().orElse(null);
+	}
+
+	/**
+	 * Finds the file number element within {@code certificateApplicationIdentifications}. Returns null if not found or
+	 * {@code certificateApplicationIdentifications} is null.
+	 */
+	@Nullable
+	@Named("findFileNumber")
+	default String findFileNumber(@Nullable Iterable<CertificateApplicationIdentificationModel> certificateApplicationIdentifications) {
+		return findCertificateApplicationIdentification(certificateApplicationIdentifications, FILE_NUMBER);
+	}
+
+	/**
+	 * Maps an {@code Iterable} to a single element by returning the first element found. Returns {@code null} if
+	 * {@code iterable} is empty or {@code null}.
+	 */
+	@Nullable
+	@Named("getFirstElement")
+	default <T> T getFirstElement(@Nullable Iterable<T> iterable) {
+		return stream(iterable).findFirst().orElse(null);
+	}
+
+	/**
+	 * A predicate that operates over {@code CertificateApplicationIdentificationModel}, returning {@code true} if
+	 * {@code CertificateApplicationIdentificationModel.identificationCategoryText} equals {@code identificationCategoryText}.
+	 * Returns {@code false} if {@code certificateApplicationIdentificationModel} is {@code null}.
+	 */
+	default Predicate<CertificateApplicationIdentificationModel> hasIdentificationCategoryText(String identificationCategoryText) {
+		Assert.hasText(identificationCategoryText, "identificationCategoryText is required; it must not be null or blank");
+		return certificateApplicationIdentification -> Optional.ofNullable(certificateApplicationIdentification)
+			.map(CertificateApplicationIdentificationModel::getIdentificationCategoryText)
+			.map(identificationCategoryText::equals)
+			.orElse(false);
+	}
+
+	/**
+	 * Maps an {@link Iterable} to a {@link Stream}. Returns {@code Stream.empty()} if {@code iterable} is null.
+	 */
+	default <T> Stream<T> stream(@Nullable Iterable<T> iterable) {
+		return Optional.ofNullable(iterable)
+			.map(Iterable::spliterator)
+			.map(spliterator -> StreamSupport.stream(spliterator, false))
+			.orElse(Stream.empty());
+	}
+
+}
