bc1e9e7ca291f45c9ed7fb069b894753ccf6b3a9
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2022-10-26 08:29:22 -0230

    Incremental commit: add event_log db table (#35)

---
+++ src/main/java/ca/gov/dtsstn/passport/api/data/entity/EventLogEntity.java
@@ -0,0 +1,132 @@
+package ca.gov.dtsstn.passport.api.data.entity;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import org.immutables.builder.Builder;
+import org.springframework.core.style.ToStringCreator;
+import org.springframework.lang.Nullable;
+
+/**
+ * @author Greg Baker (gregory.j.baker@hrsdc-rhdcc.gc.ca)
+ */
+@Entity(name = "EventLog")
+@SuppressWarnings({ "serial" })
+public class EventLogEntity extends AbstractEntity {
+
+	@Column(length = 256, nullable = false, updatable = false)
+	private String description;
+
+	@Column(length = 65536, nullable = false, updatable = false)
+	private String details;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 32, nullable = false, updatable = false)
+	private EventLogTypeEntity eventType;
+
+	@Column(length = 128, nullable = true, updatable = false)
+	private String actor;
+
+	@Column(length = 256, nullable = true, updatable = false)
+	private String source;
+
+	public EventLogEntity() {
+		super();
+	}
+
+	@Builder.Constructor
+	protected EventLogEntity( // NOSONAR (too many parameters)
+			@Nullable String id,
+			@Nullable String createdBy,
+			@Nullable Instant createdDate,
+			@Nullable String lastModifiedBy,
+			@Nullable Instant lastModifiedDate,
+			@Nullable Boolean isNew,
+			@Nullable String actor,
+			@Nullable EventLogTypeEntity eventType,
+			@Nullable String description,
+			@Nullable String details,
+			@Nullable String source) {
+		super(id, createdBy, createdDate, lastModifiedBy, lastModifiedDate, isNew);
+		this.description = description;
+		this.details = details;
+		this.eventType = eventType;
+		this.actor = actor;
+		this.source = source;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public EventLogTypeEntity getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(EventLogTypeEntity eventType) {
+		this.eventType = eventType;
+	}
+
+	public String getActor() {
+		return actor;
+	}
+
+	public void setActor(String actor) {
+		this.actor = actor;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// keeps SonarLint happy
+		return super.equals(obj);
+	}
+
+	@Override
+	public int hashCode() {
+		// keeps SonarLint happy
+		return super.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringCreator(this)
+			.append("super", super.toString())
+			.append("id", id)
+			.append("createdBy", createdBy)
+			.append("createdDate", createdDate)
+			.append("lastModifiedBy", lastModifiedBy)
+			.append("lastModifiedDate", lastModifiedDate)
+			.append("isNew", isNew)
+			.append("description", description)
+			.append("details", details)
+			.append("eventType", eventType)
+			.append("actor", actor)
+			.append("source", source)
+			.toString();
+	}
+
+}
--- src/main/resources/db-migrations/common/v0-[common]-schema-init.sql
+++ src/main/resources/db-migrations/common/v0-[common]-schema-init.sql
@@ -1,3 +1,21 @@
+CREATE TABLE event_log (
+	id VARCHAR(64) NOT NULL,
+
+	description VARCHAR(256) NOT NULL,
+	details VARCHAR(65536) NOT NULL,
+	event_type VARCHAR(32) NOT NULL,
+	actor VARCHAR(128),
+	source VARCHAR(256),
+
+	-- audit fields
+	created_by VARCHAR(64) NOT NULL,
+	created_date TIMESTAMP NOT NULL,
+	last_modified_by VARCHAR(64) NULL,
+	last_modified_date TIMESTAMP NULL,
+
+	CONSTRAINT pk_event_log PRIMARY KEY (id)
+);
+
 CREATE TABLE email_request (
 	id VARCHAR(64) NOT NULL,
 
