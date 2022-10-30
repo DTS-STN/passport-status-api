10cc630ed404f93687f125e5bab8a80d468edd5d
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-10-30 17:29:14 -0230

    Remove bad NULL syntax to prepare to move to derby

--- src/main/resources/db-migrations/common/v0-[common]-schema-init.sql
+++ src/main/resources/db-migrations/common/v0-[common]-schema-init.sql
@@ -1,17 +1,17 @@
 CREATE TABLE event_log (
 	id VARCHAR(64) NOT NULL,
 
+	actor VARCHAR(128),
 	description VARCHAR(256) NOT NULL,
 	details VARCHAR(65536) NOT NULL,
 	event_type VARCHAR(32) NOT NULL,
-	actor VARCHAR(128) NULL,
-	source VARCHAR(256) NULL,
+	source VARCHAR(256),
 
 	-- audit fields
 	created_by VARCHAR(64) NOT NULL,
 	created_date TIMESTAMP NOT NULL,
-	last_modified_by VARCHAR(64) NULL,
-	last_modified_date TIMESTAMP NULL,
+	last_modified_by VARCHAR(64),
+	last_modified_date TIMESTAMP,
 
 	CONSTRAINT pk_event_log PRIMARY KEY (id)
 );
@@ -29,8 +29,8 @@ CREATE TABLE email_request (
 	-- audit fields
 	created_by VARCHAR(64) NOT NULL,
 	created_date TIMESTAMP NOT NULL,
-	last_modified_by VARCHAR(64) NULL,
-	last_modified_date TIMESTAMP NULL,
+	last_modified_by VARCHAR(64),
+	last_modified_date TIMESTAMP,
 
 	CONSTRAINT pk_email_request PRIMARY KEY (id)
 );
@@ -38,22 +38,22 @@ CREATE TABLE email_request (
 CREATE TABLE http_request (
 	id VARCHAR(64) NOT NULL,
 
-	principal_name VARCHAR(64) NULL,
-	request_headers VARCHAR(65536) NULL,
+	principal_name VARCHAR(64),
+	request_headers VARCHAR(65536),
 	request_method VARCHAR(8) NOT NULL,
 	request_remote_address VARCHAR(64),
 	request_uri VARCHAR(4096) NOT NULL,
 	response_headers VARCHAR(65536),
 	response_status INTEGER NOT NULL,
-	session_id VARCHAR(256) NULL,
-	time_taken_millis BIGINT NULL,
+	session_id VARCHAR(256),
+	time_taken_millis BIGINT,
 	timestamp TIMESTAMP NOT NULL,
 
 	-- audit fields
 	created_by VARCHAR(64) NOT NULL,
 	created_date TIMESTAMP NOT NULL,
-	last_modified_by VARCHAR(64) NULL,
-	last_modified_date TIMESTAMP NULL,
+	last_modified_by VARCHAR(64),
+	last_modified_date TIMESTAMP,
 
 	CONSTRAINT pk_http_request PRIMARY KEY (id)
 );
@@ -61,29 +61,29 @@ CREATE TABLE http_request (
 CREATE TABLE status_code (
 	id VARCHAR(64) NOT NULL,
 
-	code VARCHAR(16) NOT NULL,
 	cdo_code VARCHAR(8) NOT NULL,
-	description VARCHAR(256) NULL,
+	code VARCHAR(16) NOT NULL,
+	description VARCHAR(256),
 	is_active BOOLEAN NOT NULL,
 
 	-- audit fields
 	created_by VARCHAR(64) NOT NULL,
 	created_date TIMESTAMP NOT NULL,
-	last_modified_by VARCHAR(64) NULL,
-	last_modified_date TIMESTAMP NULL,
+	last_modified_by VARCHAR(64),
+	last_modified_date TIMESTAMP,
 
 	CONSTRAINT pk_status_code PRIMARY KEY (id)
 );
 
-CREATE INDEX ix_code ON status_code(code);
 CREATE INDEX ix_cdo_code ON status_code(cdo_code);
+CREATE INDEX ix_code ON status_code(code);
 
 CREATE TABLE passport_status (
 	id VARCHAR(64) NOT NULL,
 
 	application_register_sid VARCHAR(256) NOT NULL,
 	date_of_birth DATE NOT NULL,
-	email VARCHAR(256) NULL,
+	email VARCHAR(256),
 	file_number VARCHAR(32) NOT NULL,
 	first_name VARCHAR(128) NOT NULL,
 	last_name VARCHAR(128) NOT NULL,
@@ -93,8 +93,8 @@ CREATE TABLE passport_status (
 	-- audit fields
 	created_by VARCHAR(64) NOT NULL,
 	created_date TIMESTAMP NOT NULL,
-	last_modified_by VARCHAR(64) NULL,
-	last_modified_date TIMESTAMP NULL,
+	last_modified_by VARCHAR(64),
+	last_modified_date TIMESTAMP,
 
 	CONSTRAINT pk_passport_status PRIMARY KEY (id)
 );
