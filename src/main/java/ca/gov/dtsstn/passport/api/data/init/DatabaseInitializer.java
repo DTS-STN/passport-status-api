6097e831655eb4c8e5a058388857f33259b02b5a
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-10-31 22:48:16 -0230

    Code cleanup

--- src/main/java/ca/gov/dtsstn/passport/api/data/init/DatabaseInitializer.java
+++ src/main/java/ca/gov/dtsstn/passport/api/data/init/DatabaseInitializer.java
@@ -70,30 +70,30 @@ public class DatabaseInitializer {
 		passportStatusRepository.deleteAllInBatch();
 
 		log.info("Fetch all active passport status codes");
-		final var statusCodeEntities = statusCodeService.readAllByIsActive(true).stream().map(statusCodeMapper::toEntity).toList();
+		final var statusCodes = statusCodeService.readAllByIsActive(true).stream().map(statusCodeMapper::toEntity).toList();
 
 		log.info("Generating {} fake random passport statuses", generatedStatusesNumber);
 		stopWatch.reset(); stopWatch.start();
-		final var randomPassportStatuses = Stream.generate(() -> generateRandomPassportStatus(statusCodeEntities)).limit(generatedStatusesNumber).toList();
+		final var randomPassportStatuses = Stream.generate(() -> generateRandomPassportStatus(statusCodes)).limit(generatedStatusesNumber).toList();
 		partition(randomPassportStatuses, 10_000).forEach(passportStatusRepository::saveAll);
 		log.info("Fake random data created in {}ms", stopWatch.getTime());
 
 		log.info("Generating {} duplicate fake passport statuses", duplicateStatusesNumber);
 		stopWatch.reset(); stopWatch.start();
-		final var duplicatePassportStatuses = Stream.generate(() -> generateDuplicatePassportStatus(statusCodeEntities)).limit(duplicateStatusesNumber).toList();
+		final var duplicatePassportStatuses = Stream.generate(() -> generateDuplicatePassportStatus(statusCodes)).limit(duplicateStatusesNumber).toList();
 		partition(duplicatePassportStatuses, 10_000).forEach(passportStatusRepository::saveAll);
 		log.info("Duplicate fake data created in {}ms", stopWatch.getTime());
 
 		// TODO :: GjB :: use properties for these
 		log.info("Generating passport team fake passport statuses");
 		stopWatch.reset(); stopWatch.start();
-		passportStatusRepository.save(generatePassportTeamPassportStatus("Greg", "Baker", LocalDate.of(2000, 01, 01), "gregory.j.baker@hrsdc-rhdcc.gc.ca", statusCodeEntities));
-		passportStatusRepository.save(generatePassportTeamPassportStatus("Kristopher", "Charbonneau", LocalDate.of(2000, 01, 01), "kristopher.charbonneau@hrsdc-rhdcc.gc.ca", statusCodeEntities));
-		passportStatusRepository.save(generatePassportTeamPassportStatus("Maxim", "Lam", LocalDate.of(2000, 01, 01), "maxim.lam@hrsdc-rhdcc.gc.ca", statusCodeEntities));
-		passportStatusRepository.save(generatePassportTeamPassportStatus("Sébastien", "Comeau", LocalDate.of(1985, 01, 10), "sebastien.comeau@hrsdc-rhdcc.gc.ca", statusCodeEntities));
-		passportStatusRepository.save(generatePassportTeamPassportStatus("Shaun", "Laughland", LocalDate.of(2000, 01, 01), "shaun.laughland@hrsdc-rhdcc.gc.ca", statusCodeEntities));
-		passportStatusRepository.save(generatePassportTeamPassportStatus("Stefan", "O'Connell", LocalDate.of(2000, 01, 01), "stefan.oconnell@hrsdc-rhdcc.gc.ca", statusCodeEntities));
-		passportStatusRepository.save(generatePassportTeamPassportStatus("Stéphane", "Viau", LocalDate.of(2000, 01, 01), "stephane.viau@hrsdc-rhdcc.gc.ca", statusCodeEntities));
+		passportStatusRepository.save(generatePassportTeamPassportStatus("Greg", "Baker", LocalDate.of(2000, 01, 01), "gregory.j.baker@hrsdc-rhdcc.gc.ca", statusCodes));
+		passportStatusRepository.save(generatePassportTeamPassportStatus("Kristopher", "Charbonneau", LocalDate.of(2000, 01, 01), "kristopher.charbonneau@hrsdc-rhdcc.gc.ca", statusCodes));
+		passportStatusRepository.save(generatePassportTeamPassportStatus("Maxim", "Lam", LocalDate.of(2000, 01, 01), "maxim.lam@hrsdc-rhdcc.gc.ca", statusCodes));
+		passportStatusRepository.save(generatePassportTeamPassportStatus("Sébastien", "Comeau", LocalDate.of(1985, 01, 10), "sebastien.comeau@hrsdc-rhdcc.gc.ca", statusCodes));
+		passportStatusRepository.save(generatePassportTeamPassportStatus("Shaun", "Laughland", LocalDate.of(2000, 01, 01), "shaun.laughland@hrsdc-rhdcc.gc.ca", statusCodes));
+		passportStatusRepository.save(generatePassportTeamPassportStatus("Stefan", "O'Connell", LocalDate.of(2000, 01, 01), "stefan.oconnell@hrsdc-rhdcc.gc.ca", statusCodes));
+		passportStatusRepository.save(generatePassportTeamPassportStatus("Stéphane", "Viau", LocalDate.of(2000, 01, 01), "stephane.viau@hrsdc-rhdcc.gc.ca", statusCodes));
 		log.info("Passport team fake data created in {}ms", stopWatch.getTime());
 	}
 
@@ -102,7 +102,7 @@ public class DatabaseInitializer {
 		return List.copyOf(passportStatuses.stream().collect(Collectors.groupingBy(it -> counter.getAndIncrement() / size)).values());
 	}
 
-	protected PassportStatusEntity generateRandomPassportStatus(List<StatusCodeEntity> statusCodeEntities) {
+	protected PassportStatusEntity generateRandomPassportStatus(List<StatusCodeEntity> statusCodes) {
 		final var fileNumber = generateFileNumber();
 		final var firstName = generateFirstName();
 		final var lastName = generateLastName();
@@ -115,12 +115,12 @@ public class DatabaseInitializer {
 			.fileNumber(fileNumber)
 			.firstName(firstName)
 			.lastName(lastName)
-			.statusCode(generateStatusCodeEntity(statusCodeEntities))
+			.statusCode(generateStatusCode(statusCodes))
 			.statusDate(generateStatusDate(LocalDate.of(2000, 01, 01), LocalDate.now()))
 			.build();
 	}
 
-	protected PassportStatusEntity generateDuplicatePassportStatus(List<StatusCodeEntity> statusCodeEntities) {
+	protected PassportStatusEntity generateDuplicatePassportStatus(List<StatusCodeEntity> statusCodes) {
 		final var dupeString = "DUPE0000";
 		final var fileNumber = dupeString;
 		final var firstName = dupeString;
@@ -134,12 +134,12 @@ public class DatabaseInitializer {
 			.fileNumber(fileNumber)
 			.firstName(firstName)
 			.lastName(lastName)
-			.statusCode(generateStatusCodeEntity(statusCodeEntities))
+			.statusCode(generateStatusCode(statusCodes))
 			.statusDate(generateStatusDate(LocalDate.of(2000, 01, 01), LocalDate.of(2000, 01, 01)))
 			.build();
 	}
 
-	protected PassportStatusEntity generatePassportTeamPassportStatus(String firstName, String lastName, LocalDate dateOfBirth, String email, List<StatusCodeEntity> statusCodeEntities) {
+	protected PassportStatusEntity generatePassportTeamPassportStatus(String firstName, String lastName, LocalDate dateOfBirth, String email, List<StatusCodeEntity> statusCodes) {
 		final var fileNumber = generateFileNumber();
 
 		final var passportStatus = new PassportStatusEntityBuilder()
@@ -150,7 +150,7 @@ public class DatabaseInitializer {
 			.fileNumber(fileNumber)
 			.firstName(firstName)
 			.lastName(lastName)
-			.statusCode(generateStatusCodeEntity(statusCodeEntities))
+			.statusCode(generateStatusCode(statusCodes))
 			.statusDate(generateStatusDate(LocalDate.of(2000, 01, 01), LocalDate.of(2000, 01, 01)))
 			.build();
 
@@ -159,7 +159,7 @@ public class DatabaseInitializer {
 	}
 
 	protected String generateApplicationRegisterSid(String fileNumber, String firstName, String lastName) {
-		return UUID.nameUUIDFromBytes(("applicationRegisterSid" + fileNumber + firstName + lastName).getBytes()).toString();
+		return UUID.nameUUIDFromBytes(("applicationRegisterSid-%s-%s-%s".formatted(fileNumber, firstName, lastName)).getBytes()).toString();
 	}
 
 	protected LocalDate generateDateOfBirth() {
@@ -167,7 +167,7 @@ public class DatabaseInitializer {
 	}
 
 	protected String generateEmail(final String firstName, final String lastName) {
-		return stripDiacritics(firstName + "." + lastName + "@example.com").toLowerCase();
+		return stripDiacritics("%s.%s@example.com".formatted(firstName, lastName)).toLowerCase();
 	}
 
 	protected String generateFileNumber() {
@@ -179,16 +179,16 @@ public class DatabaseInitializer {
 	}
 
 	protected String generateId(String fileNumber, String firstName, String lastName) {
-		return UUID.nameUUIDFromBytes(("id" + fileNumber + firstName + lastName).getBytes()).toString();
+		return UUID.nameUUIDFromBytes(("id-%s-%s-%s".formatted(fileNumber, firstName, lastName)).getBytes()).toString();
 	}
 
 	protected String generateLastName() {
 		return faker.name().lastName();
 	}
 
-	protected StatusCodeEntity generateStatusCodeEntity(List<StatusCodeEntity> statusCodeEntities) {
-		Assert.notEmpty(statusCodeEntities, "statusCodeEntities is required; it must not be null and must contain at least one element");
-        return statusCodeEntities.get(faker.random().nextInt(statusCodeEntities.size()));
+	protected StatusCodeEntity generateStatusCode(List<StatusCodeEntity> statusCodes) {
+		Assert.notEmpty(statusCodes, "statusCodes is required; it must not be null and must contain at least one element");
+		return statusCodes.get(faker.random().nextInt(statusCodes.size()));
 	}
 
 	protected LocalDate generateStatusDate(LocalDate start, LocalDate end) {
