2e8e5e40d12c682d89ea008b3d652cd7d553e483
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2022-11-10 09:29:36 -0400

    Add http request repository filtering

--- src/main/java/ca/gov/dtsstn/passport/api/actuate/PersistentHttpTraceRepository.java
+++ src/main/java/ca/gov/dtsstn/passport/api/actuate/PersistentHttpTraceRepository.java
@@ -1,6 +1,8 @@
 package ca.gov.dtsstn.passport.api.actuate;
 
 import java.net.URI;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -14,7 +16,9 @@ import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.lang.Nullable;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.Assert;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -32,11 +36,17 @@ import ca.gov.dtsstn.passport.api.data.entity.HttpRequestEntity;
 @ConfigurationProperties("application.http-request-repository")
 public class PersistentHttpTraceRepository implements HttpTraceRepository {
 
+	private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
 	private final HttpRequestRepository httpRequestRepository;
 
 	private final HttpTraceMapper httpTraceMapper = Mappers.getMapper(HttpTraceMapper.class);
 
 	private final InMemoryHttpTraceRepository inMemoryHttpTraceRepository;
+
+	private List<AntPathRequestMatcher> includeUrls = Collections.emptyList();
+
+	private List<AntPathRequestMatcher> excludeUrls = Collections.emptyList();
 
 	@Autowired // marks this constructor as the primary one spring will use
 	public PersistentHttpTraceRepository(HttpRequestRepository httpRequestRepository) {
@@ -54,8 +64,13 @@ public class PersistentHttpTraceRepository implements HttpTraceRepository {
 	@Override
 	public void add(HttpTrace httpTrace) {
 		Assert.notNull(httpTrace, "httpTrace is required; it must not be null");
+
 		inMemoryHttpTraceRepository.add(httpTrace);
-		Optional.ofNullable(httpTraceMapper.toEntity(httpTrace)).ifPresent(httpRequestRepository::save);
+
+		Optional.ofNullable(httpTrace)
+			.filter(this::shouldPersist)
+			.map(httpTraceMapper::toEntity)
+			.ifPresent(httpRequestRepository::save);
 	}
 
 	@Override
@@ -63,9 +78,35 @@ public class PersistentHttpTraceRepository implements HttpTraceRepository {
 		return inMemoryHttpTraceRepository.findAll();
 	}
 
+	protected String getPath(HttpTrace httpTrace) {
+		return httpTrace.getRequest().getUri().getPath();
+	}
+
+	protected boolean isIncluded(HttpTrace httpTrace) {
+		return includeUrls.isEmpty() || includeUrls.stream().anyMatch(includeUrl -> antPathMatcher.match(includeUrl.getPattern(), getPath(httpTrace)));
+	}
+
+	protected boolean isNotExcluded(HttpTrace httpTrace) {
+		return excludeUrls.stream().noneMatch(excludeUrl -> antPathMatcher.match(excludeUrl.getPattern(), getPath(httpTrace)));
+	}
+
 	public void setCapacity(int capacity) {
 		Assert.isTrue(capacity >= 0, "application.http-request-repository.capacity must be greater than or equal to zero");
 		inMemoryHttpTraceRepository.setCapacity(capacity);
+	}
+
+	public void setIncludeUrls(Collection<AntPathRequestMatcher> includeUrls) {
+		Assert.notNull(includeUrls, "includeUrls is required; it must not be null");
+		this.includeUrls = List.copyOf(includeUrls);
+	}
+
+	public void setExcludeUrls(Collection<AntPathRequestMatcher> excludeUrls) {
+		Assert.notNull(excludeUrls, "excludeUrls is required; it must not be null");
+		this.excludeUrls = List.copyOf(excludeUrls);
+	}
+
+	public boolean shouldPersist(HttpTrace httpTrace) {
+		return isIncluded(httpTrace) && isNotExcluded(httpTrace);
 	}
 
 	@Mapper
