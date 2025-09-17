b1c26e1d6193acdac4c05ae9b7c0e8fc7d50b842
Ken Blanchard <ken.blanchard@hrsdc-rhdcc.gc.ca>
2025-09-17 12:58:49 -0300

    Add missing information status code (8) to database migration

---
+++ src/main/resources/db-migrations/common/v7-[common]-add-missing-information-status-code.sql
@@ -0,0 +1,4 @@
+INSERT INTO status_code
+  (id, code, cdo_code, description, is_active, created_by, created_date, last_modified_by, last_modified_date)
+VALUES
+  ('6222b2c9-764a-400e-a436-b2dfc15fff92', 'MISSING_INFORMATION', '8', 'Incomplete or missing information', true, 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP);
